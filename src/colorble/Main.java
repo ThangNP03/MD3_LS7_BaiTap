128eb8c2e56230c8d83fd6ea13bd7375ea4cc106
ThangNP03 <2003nguyenphucthang@gmail.com>
2023-03-17 09:57:26 +0700

    New

--- src/colorble/Main.java
+++ src/colorble/Main.java
@@ -1,4 +1,16 @@
 package colorble;
 
 public class Main {
+    public static void main(String[] args) {
+        Shape[] shape = new Shape[4];
+        shape[0] = new Circle();
+        shape[1] = new Rectangle();
+        shape[2] = new Square(5);
+        for (Shape sh : shape) {
+            System.out.println(sh);
+            if (sh instanceof Square) {
+                ((Square) sh).howToColor();
+            }
+        }
+    }
 }
